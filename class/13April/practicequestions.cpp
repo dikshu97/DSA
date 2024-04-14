1edd6925d0df23c1c28e729602be374ca5869933
dikshit <dikshitnegta32@gmail.com>
2024-04-14 12:54:38 +0530

    commited changes

--- class/13April/practicequestions.cpp
+++ class/13April/practicequestions.cpp
@@ -1,185 +1,183 @@
 // //question =1; traingle
-// #include <iostream>
-//     using namespace std;
-// int main() {
+#include <iostream>
+    using namespace std;
+int main() {
     
-//     int n;
-//     cin>> n;
+    int n;
+    cin>> n;
 
 
     
-// for(int row=1;row<=n;row++){
-//     for(int st=1;st<=row;st++){
-//          cout<<"* ";
-//     }
-//   cout<<"\n"<<endl;
-// }
+for(int row=1;row<=n;row++){
+    for(int st=1;st<=row;st++){
+         cout<<"* ";
+    }
+  cout<<"\n"<<endl;
+}
 
 
 
 
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // //question =2; traingle
 
 
-// #include <iostream>
-//     using namespace std;
-// int main() {
+#include <iostream>
+    using namespace std;
+int main() {
    
-//    int n;
-//    cin>>n;
+   int n;
+   cin>>n;
   
-//    for(int row=1;row<=n;row++){
-//        for(int st=1;st<=n-row+1;st++){
-//            cout<<"*";
-//        }
+   for(int row=1;row<=n;row++){
+       for(int st=1;st<=n-row+1;st++){
+           cout<<"*";
+       }
       
-//        cout<<"\n";
-//    }
+       cout<<"\n";
+   }
  
 
-//     return 0;
-// }
+    return 0;
+}
 
 // //question=3;  triangle;
 
-// // Online C++ compiler to run C++ program online
-// #include <iostream>
-//     using namespace std;
-// int main() {
-//     // Write C++ code here
-//    int n;
-//    cin>>n;
-//    int nsp=n-1;
-//    int nst=1;
+#include <iostream>
+    using namespace std;
+int main() {
+    
+   int n;
+   cin>>n;
+   int nsp=n-1;
+   int nst=1;
   
-//   for(int row=1;row<=n;row++){
-//       for(int sp=1;sp<=nsp;sp++){
-//           cout<<" ";
-//       }
-//         for(int st=1;st<=nst;st++){
-//           cout<<"*";
-//       }  
-//       cout<<"\n";
+  for(int row=1;row<=n;row++){
+      for(int sp=1;sp<=nsp;sp++){
+          cout<<" ";
+      }
+        for(int st=1;st<=nst;st++){
+          cout<<"*";
+      }  
+      cout<<"\n";
       
-//       nsp=nsp-1;
-//       nst=nst+1;
-//   }
+      nsp=nsp-1;
+      nst=nst+1;
+  }
  
 
-//     return 0;
-// }
+    return 0;
+}
 
 
-// //question =4; triangle
+// //question =4; triangle;
 
-// // Online C++ compiler to run C++ program online
-// #include <iostream>
-//     using namespace std;
-// int main() {
-//     // Write C++ code here
-//    int n;
-//    cin>>n;
-//    int nsp=n-1;
-//    int nst=1;
+
+#include <iostream>
+    using namespace std;
+int main() {
+    
+   int n;
+   cin>>n;
+   int nsp=n-1;
+   int nst=1;
   
-//   for(int row=1;row<=n;row++){
-//     for(int sp=1;sp<=nsp;sp++){
-//         cout<<" ";
-//     }
-//      for(int st=1;st<=nst;st++){
-//         cout<<"* ";
-//     }
-//     cout<<"\n";
+  for(int row=1;row<=n;row++){
+    for(int sp=1;sp<=nsp;sp++){
+        cout<<" ";
+    }
+     for(int st=1;st<=nst;st++){
+        cout<<"* ";
+    }
+    cout<<"\n";
     
-//     nsp=nsp-1;
-//     nst=nst+2;
+    nsp=nsp-1;
+    nst=nst+2;
     
     
-//   }
+  }
  
 
-//     return 0;
-// }
+    return 0;
+}
 
 
 // //question=5; diamond;
  
-// #include <iostream>
-//     using namespace std;
-// int main() {
-//     // Write C++ code here
-//    int n;
-//    cin>>n;
-//    int nsp=n/2;
-//    int nst=1;
+#include <iostream>
+    using namespace std;
+int main() {
+    
+   int n;
+   cin>>n;
+   int nsp=n/2;
+   int nst=1;
   
-//   for(int row=1;row<=n;row++){
-//  for(int sp=1;sp<=nsp;sp++){
-//      cout<<"  ";
-//  }
-//   for(int st=1;st<=nst;st++){
-//      cout<<"* ";
-//  }
-//     cout<<"\n";
+  for(int row=1;row<=n;row++){
+ for(int sp=1;sp<=nsp;sp++){
+     cout<<"  ";
+ }
+  for(int st=1;st<=nst;st++){
+     cout<<"* ";
+ }
+    cout<<"\n";
     
-//     if(row<n/2+1){
-//    nsp=nsp-1;
-//    nst=nst+2;
-//     }  
+    if(row<n/2+1){
+   nsp=nsp-1;
+   nst=nst+2;
+    }  
     
-//     else{
-//         nsp=nsp+1;
-//         nst=nst-2;
-//     }
+    else{
+        nsp=nsp+1;
+        nst=nst-2;
+    }
     
     
     
-//   }
+  }
  
 
-//     return 0;
-// }
+    return 0;
+}
 
 
-// //question=6; slanting line
+// //question=6; slanting line;
 
-// #include <iostream>
-//     using namespace std;
-// int main() {
-//     // Write C++ code here
-//    int n;
-//    cin>>n;
-//   int nsp=n-1;
-//   int nst=1;
+#include <iostream>
+    using namespace std;
+int main() {
+   int n;
+   cin>>n;
+  int nsp=n-1;
+  int nst=1;
   
-//   for(int row=1;row<=n;row++){
-//       for(int sp=1;sp<=nsp;sp++){
-//           cout<<" ";
-//       }
-//       for(int st=1;st<=nst;st++){
-//           cout<<"*";
-//       }
-//       cout<<"\n";
-//       nst=nst;
-//       nsp=nsp-1;
-//   }
-//     return 0;
-// }
+  for(int row=1;row<=n;row++){
+      for(int sp=1;sp<=nsp;sp++){
+          cout<<" ";
+      }
+      for(int st=1;st<=nst;st++){
+          cout<<"*";
+      }
+      cout<<"\n";
+      nst=nst;
+      nsp=nsp-1;
+  }
+    return 0;
+}
 
 
 
 //question=7; cross;
 
-// Online C++ compiler to run C++ program online
+
 #include <iostream>
     using namespace std;
 int main() {
-    // Write C++ code here
+    
    int n;
    cin>>n;
 //   int nsp=n-1;
