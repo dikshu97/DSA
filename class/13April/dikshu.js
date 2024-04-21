f1a51a67f99dcb396ac7f6b1a7362e2b2d24cd01
dikshit <dikshitnegta32@gmail.com>
2024-04-21 19:56:59 +0530

    committed

--- class/13April/dikshu.js
+++ class/13April/dikshu.js
@@ -68,3 +68,48 @@ for(let row=1;row<=5;row++){
         nsp3=nsp3-1;
     }
 }
+
+let n = 6;
+
+for (let i = 1; i <= n; i++) {
+    // Printing stars and spaces
+    for (let j = 0; j < i; j++) {
+        if (i === n) {
+            process.stdout.write('*');
+        } else {
+            if (j === 0 || j === i - 1) {
+                process.stdout.write('*');
+            } else {
+                process.stdout.write(' ');
+            }
+        }
+    }
+    console.log();
+}
+
+//number traversal------first digit
+
+let num=7846;
+ 
+    
+let nod=0;
+let temp=num;
+
+while(temp>0){
+    
+    temp=Math.floor(temp/10);
+    nod++;
+}
+console.log(nod);
+
+let div=Math.pow(10,nod-1);
+
+
+
+while(div>0){
+   let fd=Math.floor(num/div);
+   console.log(fd);
+   num=Math.floor(num%div);
+   div=Math.floor(div/10);
+   
+}
